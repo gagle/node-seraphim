4de9bc6290dbd10e3318d2f62d4f13b8d6ea59c0
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-10-08 14:21:50 +0200

    docs

--- t.js
+++ t.js
@@ -1,44 +1,58 @@
 "use strict";
 
 var seraphim = require ("./lib");
-
+var properties = require ("properties");
 var argv = require ("argp")
 		.allowUndefinedArguments ()
 		.allowUndefinedOptions ()
 		.body ().help ()
 		.argv ();
 
+var optionsProperties = {
+	path: true,
+	namespaces: true
+};
+		
 var vault = seraphim.createVault ();
+vault.extension (".properties", function (p, cb){
+	properties.parse (p, optionsProperties, cb)
+});
 
 vault.on ("error", function (error){
 	console.log (error);
 });
 
 vault.on ("end", function (){
-	console.log ("end");
-	console.log (this.get())
+	console.log (this.get ());
 });
-/*
-vault
-		.use ({ x: 100 })
-		.use ("b.json")
-		.use ("a.json")
-		.use (function (){
-			if (argv.b){
-				vault.merge ({
-					a: {
-						b: argv.b
-					}
-				});
-			}
-		});*/
 		
 vault
+		.use ({ a: { b: 1 } })
+		.use (function (cb){
+			process.nextTick (function (){
+				cb (null, { a: { b: 2 } });
+			});
+		})
 		.use (function (){
-			return 2
-		}, console.log)
-		.use ({ a: { b: "hola" } })
+			return { a: { b: 3 } };
+		})
+		.use ("a.json")
+		.use ("b.properties")
 		.use (function (){
 			var a = vault.get ("a");
-			a.b = argv.b ? argv.b : a.b;
-		})
+			a.b = argv.b !== undefined ? argv.b : a.b;
+			a.c = argv.c !== undefined ? argv.c : a.c;
+		});
+
+/*
+$ node t.js
+{ a: { b: 3, c: 2 } }
+
+node t.js --b 0 --c 3
+{ a: { b: 0, c: 3 } }
+
+$ node t.js -h
+Usage: t [options]
+
+	-h, --help                  Display this help message and exit
+*/
