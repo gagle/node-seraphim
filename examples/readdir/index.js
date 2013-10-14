c767823573202dfabdc4d325e8bd333729dba0af
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-10-14 10:53:38 +0200

    docs

--- examples/readdir/index.js
+++ examples/readdir/index.js
@@ -11,7 +11,7 @@ var yaml = require ("js-yaml");
 var seraphim = require ("../../lib");
 
 var loadConfigFiles = function (dir, cb){
-	fs.readdir (".", function (error, entries){
+	fs.readdir (dir, function (error, entries){
 		if (error) return console.error (error);
 		
 		var propertiesOptions = {
@@ -31,6 +31,12 @@ var loadConfigFiles = function (dir, cb){
 				})
 				.extension (".properties", function (p, cb){
 					properties.parse (p, propertiesOptions, cb);
+				})
+				.extension ([".yaml", ".yml"], function (p, cb){
+					fs.readFile (p, { encoding: "utf8" }, function (error, data){
+						if (error) return cb (error);
+						cb (null, yaml.safeLoad (data));
+					});
 				});
 		
 		entries.forEach (function (entry){
@@ -51,6 +57,10 @@ loadConfigFiles (".", function (error, config){
 		database: {
 			hostname: "1.2.3.4",
 			port: 1234
+		},
+		log: {
+			type: "circular",
+			backup: "1week"
 		},
 		web: {
 			hostname: "4.3.2.1",
